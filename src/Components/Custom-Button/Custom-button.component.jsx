7beafec75632d4689cf735333c4f1f55cc935863
Maaz Muhammad Khan Project Research Assistant <maaz.khan@giki.edu.pk>
2023-10-22 17:21:33 +0500

    Wallet commit

--- src/Components/Custom-Button/Custom-button.component.jsx
+++ src/Components/Custom-Button/Custom-button.component.jsx
@@ -10,10 +10,26 @@ const CustomButton = ({ children }) => {
     async function requestAccount() {
         if (window.ethereum) {
             try {
-                const accounts = await window.ethereum.request({
-                    method: 'eth_requestAccounts',
+                // Request access to accounts
+                await window.ethereum.request({ method: 'eth_requestAccounts' });
+                // Listen for changes to accounts
+                window.ethereum.on('accountsChanged', (newAccounts) => {
+                    if (newAccounts.length > 0) {
+                        // Use the first account in the array as the active account
+                        setWalletAddress(newAccounts[0]);
+                    } else {
+                        // Handle the case where no accounts are found
+                        alert('No Ethereum accounts found in MetaMask.');
+                        setWalletAddress('');
+                    }
                 });
-                setWalletAddress(accounts[0]);
+                // Get initial accounts
+                const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+                if (accounts.length > 0) {
+                    setWalletAddress(accounts[0]);
+                } else {
+                    alert('No Ethereum accounts found in MetaMask.');
+                }
             } catch (error) {
                 console.log('Error connecting to MetaMask:', error);
             }
